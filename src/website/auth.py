3ca64e105d3bb5b4672cef8583ee55edadaa771e
aadi-sun <157902537+aadi-sun@users.noreply.github.com>
2024-04-06 09:49:29 +0000

    finished index

--- src/website/auth.py
+++ src/website/auth.py
@@ -1,14 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return render_template("logout.html")
 
-@auth.route('/signup')
+@auth.route('/signup',methods=['GET','POST'])
 def signup():
     return render_template("signup.html")
