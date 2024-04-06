7f7e6698314a80285da0cc847893e0e3f618f5ec
aadi-sun <157902537+aadi-sun@users.noreply.github.com>
2024-04-06 16:40:16 +0000

    created most of the necessary files for everyone's work, follow the path of the particular page given in views.py to view the page, i am working on login/signin database

--- src/website/auth.py
+++ src/website/auth.py
@@ -1,10 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
-    data = request.form
-    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -13,4 +11,20 @@ def logout():
 
 @auth.route('/signup',methods=['GET','POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if len(email) < 4:
+            flash("Email must be greater than 4 characters", category='error')
+        elif len(name) < 2:
+            flash("Name must be greater than 2 characters", category='error')
+        elif password1 != password2:
+            flash("Passwords should match", category='error')
+        elif len(password1) < 7:
+            flash("Length of a password must be greater than or equal to 8 characters", category='error')
+        else:
+            #add user to database
+            flash("Account created!", category='success')
     return render_template("signup.html")
--- src/website/views.py
+++ src/website/views.py
@@ -3,3 +3,43 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home_page():
     return render_template("index.html")
+
+@views.route('/course1')
+def course1():
+    return render_template('course1.html')
+
+@views.route('/course2')
+def course2():
+    return render_template('course2.html')
+
+@views.route('/course3')
+def course3():
+    return render_template('course3.html')
+
+@views.route('/course4')
+def course4():
+    return render_template('course4.html')
+
+@views.route('/course5')
+def course5():
+    return render_template('course5.html')
+
+@views.route('/payment')
+def payment():
+    return render_template('payment.html')
+
+@views.route('/section1')
+def section1():
+    return render_template('section1.html')
+
+@views.route('/section2')
+def section2():
+    return render_template('section2.html')
+
+@views.route('/section3')
+def section3():
+    return render_template('section3.html')
+
+@views.route('/cart')
+def cart():
+    return render_template('cart.html')
