fbd631959f1ec1ccc7197e11914f6ca1339daa9d
aadi-sun <157902537+aadi-sun@users.noreply.github.com>
2024-04-08 21:31:22 +0000

    Sign in and login completed. Doing logout

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -15,4 +18,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created Database!')
--- src/website/models.py
+++ src/website/models.py
@@ -1,15 +1,25 @@
 from . import db   
 from flask_login import UserMixin
-from sqlalchemy.sql import func
-
-# class Note(db.Model):
-#     id = db.Column(db.integer, primary_key=True)
-#     data = db.Column(db.String(10000))
-#     date = db.Column(db.DateTime(timezone-True), default=func.now())
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     name = db.Column(db.String(150))
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True)
+    price = db.Column(db.Integer)
+    instructor_name = db.Column(db.String(150))
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Cartcourse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'), nullable=True)
+
       
