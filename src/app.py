eaac70eb1086f03e0d4314f67668b6e2fccd502c
aadi-sun <157902537+aadi-sun@users.noreply.github.com>
2024-08-25 20:08:01 +0000

    last

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
+from socket import gethostname
 #creating flask app
 
 from website import create_app
 app = create_app()
 if __name__ == '__main__':
-    app.run(debug=True)
+    if 'liveconsole' not in gethostname():
+        app.run(debug=True)
 
 
 
